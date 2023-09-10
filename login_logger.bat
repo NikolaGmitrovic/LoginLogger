9be81b59721995aa67e82c4124319f041d6bab16
NikolaG <88148408+NikolaGmitrovic@users.noreply.github.com>
2023-09-10 14:48:10 +0200

    Add files via upload

---
+++ login_logger.bat
@@ -0,0 +1,24 @@
+@echo off
+
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+set task_name="LoginLogger"
+set exe_name=login_logger.exe
+
+schtasks /create /tn %task_name% /tr "\"%~dp0%exe_name%\"" /sc onlogon
